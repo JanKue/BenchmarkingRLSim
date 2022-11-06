9b41f616db0cc556227b11c9fda5a99c7fd83345
jankue <jan.kueblbeck@student.kit.edu>
2022-11-06 14:13:31 +0100

    test params

--- envs/test_door_env.py
+++ envs/test_door_env.py
@@ -24,16 +24,16 @@ if __name__ == "__main__":
     ctrl = env.controller
 
     env.reset()
-    for i in range(500):
+    for i in range(10_000):
         action = ctrl.action_space().sample()
-        print("action:", action)
+        # print("action:", action)
         step_obs, step_reward, step_done, _ = env.step(action)
-        print("%d: Position " % (i), robot.current_c_pos)
+        # print("%d: Position " % (i), robot.current_c_pos)
         print("Latest step reward:", step_reward)
-        print("observation: ", step_obs)
-        print("valid observation: ", env.observation_space.contains(step_obs))
+        # print("observation: ", step_obs)
+        # print("valid observation: ", env.observation_space.contains(step_obs))
 
-        if step_done or (i + 1) % 50 == 0:
+        if step_done or (i + 1) % 250 == 0:
             print("reset!")
             env.reset()
 
