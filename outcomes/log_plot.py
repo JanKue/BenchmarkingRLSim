e7128682165bb9fcb4f186338c9fadb00f0058c9
jankue <jan.kueblbeck@student.kit.edu>
2023-02-11 22:25:40 +0100

    plotting standard error

--- outcomes/log_plot.py
+++ outcomes/log_plot.py
@@ -47,18 +47,18 @@ def get_dataframes(glob_path, smooth=False):
 def main():
     plt.close('all')
 
-    sac_data_concat = get_dataframes("./cluster/door_open_task/door_open_sac_experiment/**/events.*")
-    ppo_data_concat = get_dataframes("./cluster/door_open_task/door_open_ppo_experiment/**/events.*")
-    ddpg_data_concat = get_dataframes("./cluster/door_open_task/door_open_ddpg_experiment/**/events.*")
-    td3_data_concat = get_dataframes("./cluster/door_open_task/door_open_td3_experiment/**/events.*")
+    sac_data_concat = get_dataframes("./cluster/door_open_task_easy/door_open_easy_sac_experiment/**/events.*", True)
+    ppo_data_concat = get_dataframes("./cluster/door_open_task_easy/door_open_easy_ppo_experiment/**/events.*", True)
+    ddpg_data_concat = get_dataframes("./cluster/door_open_task_easy/door_open_easy_ddpg_experiment/**/events.*", True)
+    td3_data_concat = get_dataframes("./cluster/door_open_task_easy/door_open_easy_td3_experiment/**/events.*", True)
 
     plt.figure()
     plt.xlim(0, 5e6)
     plt.ylim(0, 1)
-    sns.lineplot(sac_data_concat, x='step', y='value', estimator='mean', errorbar=('sd', 1), label='SAC')
-    sns.lineplot(ppo_data_concat, x='step', y='value', estimator='mean', errorbar=('sd', 1), label='PPO')
-    sns.lineplot(ddpg_data_concat, x='step', y='value', estimator='mean', errorbar=('sd', 1), label='DDPG')
-    sns.lineplot(td3_data_concat, x='step', y='value', estimator='mean', errorbar=('sd', 1), label='TD3')
+    sns.lineplot(sac_data_concat, x='step', y='value', estimator='mean', errorbar='se', label='SAC')
+    sns.lineplot(ppo_data_concat, x='step', y='value', estimator='mean', errorbar='se', label='PPO')
+    sns.lineplot(ddpg_data_concat, x='step', y='value', estimator='mean', errorbar='se', label='DDPG')
+    sns.lineplot(td3_data_concat, x='step', y='value', estimator='mean', errorbar='se', label='TD3')
 
     # grouped_df = concat_df.groupby(concat_df.index)
 
@@ -66,9 +66,9 @@ def main():
     #     df['value'] = gaussian_filter1d(df['value'], sigma=2)
     #     sns.lineplot(data=df, x='step', y='value').set_title('TD3: success rate')
 
-    plt.savefig(fname="./plots/door_success_rates_smooth.svg")
+    plt.savefig(fname="./plots/door_easy_success_rates_smooth.svg")
     tpl.clean_figure()
-    tpl.save(filepath="./plots/door_success_rates_smooth.tex")
+    tpl.save(filepath="./plots/door_easy_success_rates_smooth.tex")
 
     plt.show()
 
