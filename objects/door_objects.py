82005f74581e3cc3e0dee7d14997e23c84094f16
jankue <jan.kueblbeck@student.kit.edu>
2023-02-26 17:38:22 +0100

    added object files (xml derived from Meta-World)

---
+++ objects/door_objects.py
@@ -0,0 +1,22 @@
+from abc import ABC
+
+from alr_sim.core.sim_object.sim_object import SimObject
+from alr_sim.sims.mujoco.MujocoLoadable import MujocoXmlLoadable
+
+
+class DoorObjects(SimObject, MujocoXmlLoadable, ABC):
+
+    def __init__(self,
+                 name: str,
+                 init_pos=[0.0, 0.0, 0.0],
+                 init_quat=[0.0, 0.0, 0.0, 0.0]):
+        super(DoorObjects, self).__init__(name, init_pos, init_quat)
+
+        path_to_alr_sim_framework = "/home/jan/SimulationFramework"
+        self.xml_file_path = path_to_alr_sim_framework + "/models/mujoco/objects/door_objects.xml"
+
+    def xml_file_path(self):
+        return self.xml_file_path
+
+    def get_poi(self) -> list:
+        return [self.name]
